a278a71968f90c0f855b9f031609b859fa93285f
Pablo <vilelapege1113@gmail.com>
2025-03-13 00:58:14 -0300

    Header finalizado

--- teste-front-end/src/components/header/BottomBar.tsx
+++ teste-front-end/src/components/header/BottomBar.tsx
@@ -2,34 +2,37 @@ import React from "react";
 
 function BottomBar() {
     return <div>
-        <nav>
-            <ul>
+        <nav className="BottomBarBox">
+            <ul className="BottomBarOpcoes">
                 <li>
-                    
+                    <a href="/TodasAsCategorias" aria-label="Todas as categorias">TODAS AS CATEGORIAS</a>
                 </li>
 
                 <li>
-
+                    <a href="/Supermercado" aria-label="Supermercado">SUPERMERCADO</a>
                 </li>
 
                 <li>
-
+                    <a href="/Livros" aria-label="Livros">LIVROS</a>
                 </li>
 
                 <li>
-
+                    <a href="/Moda" aria-label="Moda">MODA</a>
                 </li>
 
                 <li>
-
+                    <a href="/Lançamentos" aria-label="Lançamentos">LANÇAMENTOS</a>
                 </li>
 
                 <li>
-
+                    <a href="/OfertasDoDia" aria-label="Ofertas do dia" className="OfertaDoDia">OFERTAS DO DIA</a>
                 </li>
 
                 <li>
-
+                    <a href="/Assinatura" aria-label="Assinatura">
+                        <img src="/Assets/CrownSimple.png" alt="" />
+                        ASSINATURA
+                    </a>
                 </li>
 
             </ul>
