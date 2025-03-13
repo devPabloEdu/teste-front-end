ba3be517e0142e8ecd999585dd35ec4559755fcd
Pablo <vilelapege1113@gmail.com>
2025-03-13 01:49:52 -0300

    Adicionando Acessibilidade

--- teste-front-end/src/components/Banner/BannerContent.tsx
+++ teste-front-end/src/components/Banner/BannerContent.tsx
@@ -3,8 +3,10 @@ import React from "react";
 function BannerContent() {
     return <article className="BannerContentBox">
         <h1>Venha conhecer nossas promoções</h1>
+
         <h2><span>50% Off</span> nos produtos</h2>
-        <button>Ver Produtos</button>
+
+        <button aria-label="Ver Produtos">Ver Produtos</button>
     </article>
 }
 
